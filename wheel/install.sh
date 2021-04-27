4304252b47b183930c2e86c577158fe4f0a744a3
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-04-27 14:41:07 +0530

    Update install.sh

--- wheel/install.sh
+++ wheel/install.sh
@@ -9,4 +9,6 @@ shift
 
 docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2010_x86_64 /io/wheel/build.sh ${pkg} ${versions}
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux1_i686 /io/wheel/build.sh ${pkg} ${versions}
 docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2014_aarch64 /io/wheel/build.sh ${pkg} ${versions}
