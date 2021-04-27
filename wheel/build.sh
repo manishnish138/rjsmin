2992e0e3ede77041ff437c258f975ad82b784545
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-04-27 10:36:41 +0530

    Update build.sh

--- wheel/build.sh
+++ wheel/build.sh
@@ -31,9 +31,8 @@ for dir in /opt/python/*; do
     pyv="$(
         "${dir}/bin/python" -c 'import sys; sys.stdout.write("".join(map(str, sys.version_info[:2])))'
     )"
-    echo "printing"
-    echo "${pyv}"
-    echo "${versions/${pyv}"
+    check="${pyv}"
+    test="${versions/${pyv}"
     if [ "${versions/${pyv}}" = "${versions}" ]; then
         continue
     fi
