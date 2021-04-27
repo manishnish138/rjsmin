de8888b9f923cca992d19ae1a6a4d5ccf29aba6e
manishnish138 <82148691+manishnish138@users.noreply.github.com>
2021-04-27 12:20:04 +0530

    Update install.sh

--- wheel/install.sh
+++ wheel/install.sh
@@ -2,3 +2,4 @@
 set -ex
 
 docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2010_x86_64 /io/wheel/build.sh rjsmin 1.1.2
+docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux2014_aarch64 /io/wheel/build.sh rjsmin 1.1.2
